eba85635dd1aea3e068e2cb24c354d998831be6b
= <campbemg@uw.edu>
2017-09-03 13:53:43 -0700

    Dont process genes and variations before tokenization   -Genes and variations are no longer stemmed   -Genes and variations are no longer lowercase   -changing words to lowercase now occurs in porter_tokenizer method

--- test/unittests.py
+++ test/unittests.py
@@ -5,7 +5,6 @@ run `make test`
 '''
 from __future__ import division
 import unittest as unittest
-import numpy as np
 from src import preprocessing
 
 
@@ -13,24 +12,35 @@ class TestPreprocessing(unittest.TestCase):
 
     def test_remove_fig(self):
         result = preprocessing.clean_text("From fig. 1 we see")
-        self.assertEqual(result,"from  we see")
+        self.assertEqual(result, "From  we see")
 
     def test_remove_figure(self):
         result = preprocessing.clean_text("see Figure A")
-        self.assertEqual(result,"see")
+        self.assertEqual(result, "see")
 
     def test_remove_number(self):
         result = preprocessing.clean_text("the result increased by 22.7%")
-        self.assertEqual(result,"the result increased by")
+        self.assertEqual(result, "the result increased by")
 
     def test_remove_number_comma(self):
         result = preprocessing.clean_text("the numbers range from 1,2,3")
-        self.assertEqual(result,"the numbers range from")
+        self.assertEqual(result, "the numbers range from")
 
     def test_dont_remove_gene(self):
         result = preprocessing.clean_text("the gene WK320 is associated with")
-        self.assertEqual(result,"the gene wk320 is associated with")
+        self.assertEqual(result, "the gene WK320 is associated with")
 
     def test_remove_letter_reference(self):
         result = preprocessing.clean_text("from [a] we see that [B] should")
-        self.assertEqual(result,"from  we see that  should")
+        self.assertEqual(result, "from  we see that  should")
+
+    def test_process_words(self):
+        genes, variations = preprocessing.get_genes_variants(
+                            'data/training_variants.csv',
+                             'data/test_variants.csv')
+        self.assertIn('CIC', genes)
+        self.assertIn('DHCR7', genes)
+        self.assertIn('W802*', variations)
+        self.assertIn('L399V', variations)
+        
+
